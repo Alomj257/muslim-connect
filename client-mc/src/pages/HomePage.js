ed937f1ba70760f48e697f70a778ba688ce6e8b4
Jahangir Alom <alomj257@gmail.com>
2024-04-16 01:12:39 +0530

    Remove homepage

--- client-mc/src/pages/HomePage.js
+++ client-mc/src/pages/HomePage.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import PopularService from "../components/Popular_Service";
+//import Navbar from "../components/Navbar";
+//import PopularService from "../components/Popular_Service";
 function HomePage() {
   return (
     <div>
-      <Navbar />
-      <PopularService />
+      {/* <Navbar />
+      <PopularService /> */}
     </div>
   );
 }
--- server-mc/Config/DBConnect.js
+++ server-mc/Config/DBConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-// const URL = "mongodb+srv://alomj257:alomj257@cluster0.eesendj.mongodb.net/TSW";
+const URL = "mongodb+srv://alomj257:alomj257@cluster0.eesendj.mongodb.net/muslim-connect";
 const Connection = async () => {
   try {
     await mongoose.connect(process.env.DB_URL || URL);
