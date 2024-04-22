fbaa897fbb9d806d5472db226e33edb90320cbb6
SaugataDutta <duttarohit786@gmail.com>
2024-04-23 00:30:28 +0530

    [UPD]Student Page

--- client-mc/src/components/Item/Item.jsx
+++ client-mc/src/components/Item/Item.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import "./Item.css"
+import cartImg from "../../assets/Student/cartImg.png"
+import profileImg from "../../assets/Student/Ellipse 21.png"
+import starImg from "../../assets/Student/material-symbols_star.png"
 const Item = () => {
     return (
-        <div className="card">
+        <div className="card item">
             <div class="cart-item">
-                <img src="product-image.jpg" alt="Product Image" />
+                <img src={cartImg} alt="Product Image" />
                 <div class="cart-item-info">
-                    <h4>Product Name</h4>
-                    <p>Product Description</p>
-                    <p>Quantity: 1</p>
+                    <div>
+                        <img src={profileImg} className="profileImg" />
+                        <p className="cart-item-price" style={{ fontSize: "14px", marginLeft: "-80px" }}>Ayesha Ali</p>
+                        <p class="cart-item-price">$19.99</p>
+                    </div>
+
+                    <p className="itemDesc">I will give consultation on the
+                        Financial system in light of Quran</p>
+                    <div className="rating">
+                        <img src={starImg} alt="Star" className="star" />
+                        {/* <span className="rating-count">(2 reviews)</span> */}
+                    </div>
                 </div>
-                <div class="cart-item-price">$19.99</div>
-                {/* <div class="cart-item-actions">
-                <button>Edit</button>
-                <button>Remove</button>
-            </div> */}
             </div>
         </div>
     )
