002f1f3046ea84be605c51ee022e47247cf81e00
Md <shoyebmd424@gmail.com>
2024-05-31 19:38:25 +0530

    deploy

--- server-mc/Config/expressConfig.js
+++ server-mc/Config/expressConfig.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.use("/profile/pic", express.static(path.join(__dirname, "../public/User")));
 app.use(
@@ -24,5 +25,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Internal server error");
 });
-
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 module.exports = app;
